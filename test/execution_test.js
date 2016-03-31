dc889cb5720cb6406ee72155c288e6fc2482186f
Yuriy Ankudinov <stremlenye@gmail.com>
2016-03-31 14:07:10 +0200

    Add test to catch redundand question mark appearence

--- test/execution_test.js
+++ test/execution_test.js
@@ -9,15 +9,20 @@ describe('Http GET', () => {
       const method = 'POST'
       const obj = {some: 'body'}
       const executor = (u, m, headers, responseType, body) => new Promise(_ => {
-        expect(u).to.equal('http://localhost:3000/test?ping=pong')
-        expect(m).to.equal(method)
-        expect(headers).to.eql([
-          ['Content-Type', 'application/json;charset=UTF-8'],
-          ['Accept', 'application/json']
-        ])
-        expect(responseType).to.be.equal('json')
-        expect(body).to.be.equal(JSON.stringify(obj))
-        done()
+        try {
+          expect(u).to.equal('http://localhost:3000/test?ping=pong')
+          expect(m).to.equal(method)
+          expect(headers).to.eql([
+            ['Content-Type', 'application/json;charset=UTF-8'],
+            ['Accept', 'application/json']
+          ])
+          expect(responseType).to.be.equal('json')
+          expect(body).to.be.equal(JSON.stringify(obj))
+        } catch(e) {
+          done(e)
+        } finally {
+          done()
+        }
       })
       const client = (new Http()).withMethod(method)
         .executor(executor)
@@ -34,20 +39,62 @@ describe('Http GET', () => {
   })
 
   describe('Current API', () => {
-    it('execute simple get request', (done) => {
+    it('execute simple get request without query', (done) => {
+      const url = 'http://localhost:3000/:some_segment'
+      const method = 'GET'
+      const executor = (u, m, headers, responseType, body) => new Promise(_ => {
+        try {
+          expect(u).to.equal('http://localhost:3000/test')
+          expect(m).to.equal(method)
+          expect(headers).to.eql([
+            ['Content-Type', 'application/json;charset=UTF-8'],
+            ['Accept', 'application/json']
+          ])
+          expect(responseType).to.be.equal('json')
+          expect(body).to.be.equal(JSON.stringify(obj))
+        } catch(e) {
+          done(e)
+        } finally {
+          done()
+        }
+      })
+      const client = (new Http()).withMethod(method)
+        .executor(executor)
+        .url(url)
+        .segment('some_segment', 'test')
+        .header('Content-Type', 'application/json;charset=UTF-8')
+        .header('Accept', 'application/json')
+        .responseType('json')
+        .responseProcessor(response => {
+          switch (response.code) {
+            case "404":
+              return { message: 'Resource not found' }
+            default:
+              return response
+          }
+        })
+      return client.exec()
+    })
+
+    it('execute simple post request', (done) => {
       const url = 'http://localhost:3000/:some_segment'
       const method = 'POST'
       const obj = {some: 'body'}
       const executor = (u, m, headers, responseType, body) => new Promise(_ =>{
-        expect(u).to.equal('http://localhost:3000/test?ping=pong')
-        expect(m).to.equal(method)
-        expect(headers).to.eql([
-          ['Content-Type', 'application/json;charset=UTF-8'],
-          ['Accept', 'application/json']
-        ])
-        expect(responseType).to.be.equal('json')
-        expect(body).to.be.equal(JSON.stringify(obj))
-        done()
+        try {
+          expect(u).to.equal('http://localhost:3000/test?ping=pong')
+          expect(m).to.equal(method)
+          expect(headers).to.eql([
+            ['Content-Type', 'application/json;charset=UTF-8'],
+            ['Accept', 'application/json']
+          ])
+          expect(responseType).to.be.equal('json')
+          expect(body).to.be.equal(JSON.stringify(obj))
+        } catch(e) {
+          done(e)
+        } finally {
+          done()
+        }
       })
       const client = (new Http()).withMethod(method)
         .executor(executor)
