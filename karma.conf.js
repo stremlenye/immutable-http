e98e244271470321e27fe782b1fff268ac7565c4
Yuriy Ankudinov <stremlenye@gmail.com>
2016-04-01 16:08:11 +0200

    test over firefox

--- karma.conf.js
+++ karma.conf.js
@@ -69,8 +69,7 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
       'Chrome',
-      'Firefox',
-      'IE'
+      'Firefox'
     ]
   })
 }
